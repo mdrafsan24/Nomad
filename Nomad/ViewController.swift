9acbf722f830010868f9481b9d2e26462c48d55f
MD R CHOWDHURY <mdrafsan@buffalo.edu>
2020-10-24 23:41:00 -0400

    Initial Commit

---
+++ Nomad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nomad
+//
+//  Created by MD R CHOWDHURY on 10/24/20.
+//  Copyright © 2020 MD R CHOWDHURY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
