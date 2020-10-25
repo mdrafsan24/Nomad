4b75823ac7a08b872b6e7d575f69cc2b620c24e2
Anant Patni <32172850+anantpat@users.noreply.github.com>
2020-10-25 12:23:57 -0400

    FINAL COMMIT

---
+++ Nomad/AppID.swift
@@ -0,0 +1,10 @@
+//
+//  AppID.swift
+//  Pewtor
+//
+//  Created by MD R CHOWDHURY on 5/30/19.
+//  Copyright © 2019 MD R CHOWDHURY. All rights reserved.
+//
+
+var AppID: String = "6e861dce3df94d7a830df040a723e210"                  // Tutorial Step 1
+// FOR RTC ENGINE
