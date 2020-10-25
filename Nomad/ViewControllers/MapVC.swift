52bbe877c88cee0e7fc27f669d99c429b4e6610f
MD R CHOWDHURY <mdrafsan@buffalo.edu>
2020-10-25 01:10:40 -0400

    UI HomePage + Initial Icon

---
+++ Nomad/ViewControllers/MapVC.swift
@@ -0,0 +1,30 @@
+//
+//  MapVC.swift
+//  Nomad
+//
+//  Created by MD R CHOWDHURY on 10/25/20.
+//  Copyright © 2020 MD R CHOWDHURY. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
